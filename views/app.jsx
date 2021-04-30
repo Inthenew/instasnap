48f05e7eb5ad5b467800c3f28968a42c134b87b2
Inthenew <38168276+Inthenew@users.noreply.github.com>
2021-04-30 15:43:01 -0400

    Add files via upload

---
+++ views/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+class App extends Reacr.Component {
+    render () {
+        return (
+            <Button bsStyle="success" bsSize="large" href="https://code.jquery.com/jquery-3.5.1.js" target="_blank">View jquery</Button>
+        )
+    }
+}
+export default App;
